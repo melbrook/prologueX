726f7161674791b45d4cb041ce4b0a337e255f64
medjed <trelllawney@gmail.com>
2024-10-26 22:51:51 -0600

    message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import { BsArrowDown, BsArrowUp } from 'react-icons/bs';
 
 function App() {
+
+  const scrollTop = (() => window.scrollTo({top: 0, behavior: 'smooth'}));
+  const scrollBottom =(() => window.scrollTo({left: 0, top: document.body.scrollHeight, behavior:'smooth'}))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='head'>
+        Prologue 
+        </h1>
+        <div className="timestamps">
+
+        <p className='timestamp'>
+        September 26, 2024, 11:23 PM, CST
+
+        </p>
+        <p className='timestamp'>
+      September 27, 2024, 3:36 PM, GMT+7
+
+        </p>
+        </div>
        <Home />
-      </header>
+ 
+
+       <footer className='footr'>
+        <span>You already know</span>
+       </footer>
+       <div className="arrows">
+        <span onClick={scrollTop}>
+        <BsArrowUp size={18}/> 
+        </span>
+        <span className='arrow-bottom' onClick={scrollBottom}>
+          <BsArrowDown size={18}/>
+        </span>
+       </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 import messages from "./data";
+import "./App.css";
+let formattedMessages = '';
+let linebreak = document.createElement('br');
+let myRegex = "(^\\.[^\\.]|[^\\.]\\.[^\\.]|[^\\.]\\.$)";
+let substitution = "$&\n";
 
 function Home() {
+
+  const alternatingColor = ['#61dafb', '#a9a9a9'];
+  const classUsed = ['kar', 'al'];
 
   const getMessages = () => {
     const messageList = [];
@@ -14,23 +22,25 @@ function Home() {
 
   let myMessageArr = getMessages();
 
-  const allMessages = myMessageArr.map((message) => {
+  const allMessages = myMessageArr.map((message, index) => {
+    let formattedMessage = message.text;
+    //let formattedMessage = message.text.split(".").join("<br/>");
+
     return (
           <div
-            style={{
-              display: "flex",
-              marginInline: "auto",
-            }}
-          >
-            {message.text}
+          className={index % 2 ? 'bubble-a' : 'bubble-k'}>
+            <span className="bubble-text">
+            {formattedMessage}
+            </span>
           </div>
     );
   });
 
+
+
   return (
     <>
       <section className="section home">
-        <hr className="horizontal-line" />
          <ul className="messages-list">{allMessages}</ul>
       </section>
     </>
